f05270d712ac7547512c8a773735466a29480cef
FaziletKosure <faziletkosure1@gmail.com>
2020-09-17 18:39:10 +0200

    add total price

--- index.js
+++ index.js
@@ -67,153 +67,33 @@ function update2(e){
     if(e=='+'){
         counter = counter+1;
         console.log('counter',counter);
-        normalPrice2 +=  np2c;
-        discountPrice2 +=dp2c;
+        normalPrice2 =  np2c*counter;
+        discountPrice2 =dp2c*counter;
         total=discountPrice1+discountPrice2;
     }
-    else if(e=='-')
-    {
+    else if(e=='-'){
         if(counter > 1){
-            counter -=  1;
-            normalPrice2 =  np2c*counter;
+            counter -= 1;
+            normalPrice2 = np2c*counter;
             discountPrice2 =dp2c*counter;
             total=discountPrice1+discountPrice2;
         }
         else if(counter == 1){
             counter -=1;
             total=discountPrice1+0;
-            
             normalPrice2 =  np2c;
             discountPrice2 =dp2c;
+        }else if(counter==0){
+            counter =0
+            total=discountPrice1+0;
         }
        
     }
     document.getElementsByClassName('counter')[1].innerText = counter;
     document.getElementById('price2-discounted').innerHTML=discountPrice2.toFixed(2);
     document.getElementsByClassName('price2')[0].innerText=normalPrice2.toFixed(2);
-    document.getElementsByClassName('total')[0].innerText=(total+shipping).toFixed(2);
+    document.getElementsByClassName('total')[0].innerText=(total).toFixed(2);
 
 };
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-function CounterUpdate(action) {
-    const salePrice  = 54.99;
-    const normalPrice = 94.99;
-
-    let counter = document.getElementById('counter').innerHTML;
-    let parsed = parseInt(counter);
-    let result =parsed ;
-
-    let price1 = document.getElementById('sale-price-1').innerHTML;
-    let parsed2 = parseFloat(price1);
-    let salePriceResult = parsed2;
-    
-    let price2 = document.getElementById('normal-price-1').innerHTML;
-    let parsed3 = parseFloat(price2);
-    let normalPriceResult = parsed3;
-
-
-    if(action=='+')
-    {
-      result = parsed +1;
-      salePriceResult +=  salePrice;
-      normalPriceResult += normalPrice;
-    }
-    else if(action=='-')
-    {
-        if(parsed != 1){
-            result = parsed -1;
-            salePriceResult -=  salePrice;
-            normalPriceResult -= normalPrice;
-        }
-        else if(parsed == 1){
-            
-            salePriceResult =  salePrice;
-            normalPriceResult = normalPrice;
-        }
-       
-    }
-    document.getElementById('counter').innerHTML = result;
-    document.getElementById('sale-price-1').innerHTML = salePriceResult.toFixed(2);
-    document.getElementById('normal-price-1').innerHTML = normalPriceResult.toFixed(2);
-
-}
-
-// function CounterUpdate2(action) {
-//     var counter = document.getElementById('counter2').innerHTML;
-//     var parsed = parseInt(counter);
-//     var result =parsed ;
-//     if(action=='+')
-//     {
-//       result = parsed +1;
-//     }
-//     else if(action=='-')
-//     {
-//         if(parsed > 0){
-//             result = parsed -1;
-//         }
-      
-//     }
-//     document.getElementById('counter2').innerHTML = result;
-// }
-function CounterUpdate2(action) {
-    const salePrice  = 74.99;
-    const normalPrice = 124.99;
-    
-
-    let counter = document.getElementById('counter2').innerHTML;
-    let parsed = parseInt(counter);
-    let result =parsed ;
-
-    let price1 = document.getElementById('sale-price-2').innerHTML;
-    let parsed2 = parseFloat(price1);
-    let salePriceResult = parsed2;
-    
-    let price2 = document.getElementById('normal-price-2').innerHTML;
-    let parsed3 = parseFloat(price2);
-    let normalPriceResult = parsed3;
-
-
-    if(action=='+')
-    {
-      result = parsed +1;
-      salePriceResult +=  salePrice;
-      normalPriceResult += normalPrice;
-    }
-    else if(action=='-')
-    {
-        if(parsed != 1){
-            result = parsed -1;
-            salePriceResult -=  salePrice;
-            normalPriceResult -= normalPrice;
-        }
-        else if(parsed == 1){
-            
-            salePriceResult =  salePrice;
-            normalPriceResult = normalPrice;
-        }
-       
-    }
-    
-    document.getElementById('counter2').innerHTML = result;
-    document.getElementById('sale-price-2').innerHTML = + salePriceResult.toFixed(2);
-    document.getElementById('normal-price-2').innerHTML =  + normalPriceResult.toFixed(2);
-    
-}
-function totalPayment() {
-    let shipping = 19 ;
-
-    let price1 = document.getElementById('sale-price-1').innerHTML;
-    let parsed2 = parseFloat(price1);
-    let prudoct1 = parsed2;
-    
-    let price2 = document.getElementById('sale-price-2').innerHTML;
-    let parsed3 = parseFloat(price2);
-    let prudoct2 = parsed3;
-    
-    document.getElementById('total-payment').innerHTML = (prudoct1+prudoct2+shipping).toFixed(2);
-}
-
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = 5;
-//   }
